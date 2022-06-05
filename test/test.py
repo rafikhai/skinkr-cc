06b84a7fe4f4b6a0f1350949d8031917c73277fe
ardlyansyahh <ardlyansyah556@gmail.com>
2022-06-05 12:40:36 +0700

    Upload file

--- test/test.py
+++ test/test.py
@@ -1,11 +1,10 @@
 import requests
 import base64
 
-f = open('test/f22.jpg', 'rb')
-img = f.read()
-b64 = base64.b64encode(img)
-print(b64)
+import base64
 
-resp = requests.post("http://127.0.0.1:5000")
+with open("test/p8.jpg", "rb") as img_file:
+    b64_string = base64.b64encode(img_file.read())
 
-# print(resp.json())
+resp = requests.post("http://127.0.0.1:5000", json={'base64': b64_string.decode('utf-8')})
+print(resp.json())
