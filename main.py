31f25b249fe9bd6f05c4d238425495cb4ee69b50
ardlyansyahh <ardlyansyah556@gmail.com>
2022-06-06 12:39:14 +0700

    file upload fix

--- main.py
+++ main.py
@@ -6,10 +6,7 @@ from tensorflow import keras
 from PIL import Image
 from flask import Flask, request, jsonify
 import base64
-
-# from google.cloud import storage
 from io import BytesIO
-# import pandas as pd
 
 
 app = Flask(__name__)
@@ -17,7 +14,6 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        # content = request.json
         file = request.get_json('file')
         imagefix = Image.open(BytesIO(base64.b64decode(file['base64'])))
         if file['base64'] == "":
@@ -25,43 +21,7 @@ def index():
         try: 
                 model = keras.models.load_model("Model.h5")
                 train_dir = os.path.join('dataset/train')
-                validation_dir = os.path.join('dataset/test')
-
-                train_blackhead_dir = os.path.join(train_dir, 'blackhead')
-                train_folikulitis_dir = os.path.join(train_dir, 'folikulitis')
-                train_melasma_dir = os.path.join(train_dir, 'melasma')
-                train_nodules_dir = os.path.join(train_dir, 'nodules')
-                train_papula_dir = os.path.join(train_dir, 'papula')
-                train_pustula_dir = os.path.join(train_dir, 'pustula')
-                train_rosacea_dir = os.path.join(train_dir, 'rosacea')
-                train_whitehead_dir = os.path.join(train_dir, 'whitehead')
-                train_normalface_dir = os.path.join(train_dir, 'normalface')
-
-                    # Directory with our validation acne pictures
-                validation_blackhead_dir = os.path.join(validation_dir, 'blackhead')
-                validation_folikulitis_dir = os.path.join(validation_dir, 'folikulitis')
-                validation_melasma_dir = os.path.join(validation_dir, 'melasma')
-                validation_nodules_dir = os.path.join(validation_dir, 'nodules')
-                validation_papula_dir = os.path.join(validation_dir, 'papula')
-                validation_pustula_dir = os.path.join(validation_dir, 'pustula')
-                validation_rosacea_dir = os.path.join(validation_dir, 'rosacea')
-                validation_whitehead_dir = os.path.join(validation_dir, 'whitehead')
-                validation_normalface_dir = os.path.join(validation_dir, 'normalface')
-                train_blackhead_fnames = os.listdir(train_blackhead_dir)
-                train_folikulitis_fnames = os.listdir(train_folikulitis_dir)
-                train_melasma_fnames = os.listdir(train_melasma_dir)
-                train_nodules_fnames = os.listdir(train_nodules_dir)
-                train_papula_fnames = os.listdir(train_papula_dir)
-                train_pustula_fnames = os.listdir(train_pustula_dir)
-                train_rosacea_fnames = os.listdir(train_rosacea_dir)
-                train_whitehead_fnames = os.listdir(train_whitehead_dir)
-                train_normalface_fnames = os.listdir(train_normalface_dir)
-
-                # model = keras.models.load_model("Model.h5")
-                # model.summary()
                 from keras.preprocessing.image import ImageDataGenerator
-
-                    # All images will be rescaled by 1./255.
                 train_datagen = ImageDataGenerator(
                     rescale = 1.0/255.,
                     width_shift_range=0.2,
@@ -71,24 +31,10 @@ def index():
                     zoom_range=0.2,
                     horizontal_flip=True,
                     fill_mode='nearest')
-                test_datagen  = ImageDataGenerator(
-                    rescale = 1.0/255.)
-
-                    # --------------------
-                    # Flow training images in batches of 27 using train_datagen generator
-                    # --------------------
                 train_generator = train_datagen.flow_from_directory(train_dir,
                                                                         batch_size=27,
                                                                         class_mode='categorical',
                                                                         target_size=(224, 224))     
-                # --------------------
-                # Flow validation images in batches of 22 using test_datagen generator
-                # --------------------
-                validation_generator =  test_datagen.flow_from_directory(validation_dir,
-                                                                            batch_size=22,
-                                                                            class_mode  = 'categorical',
-                                                                            target_size = (224, 224))
-
 
                 class_dict = {v : k for k, v in train_generator.class_indices.items()}
                 imagefix = tf.image.resize(imagefix, [224, 224])
@@ -103,7 +49,6 @@ def index():
             return jsonify({"error": str(e)})
 
     return "OK"
-
 
 if __name__ == "__main__":
      app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
--- test/test.py
+++ test/test.py
@@ -4,5 +4,5 @@ import base64
 with open("test/p8.jpg", "rb") as img_file:
     b64_string = base64.b64encode(img_file.read())
 
-resp = requests.post("http://127.0.0.1:8080", json={'base64': b64_string.decode('utf-8')})
+resp = requests.post("https://skinkrcc-mzlcbhc7tq-uc.a.run.app", json={'base64': b64_string.decode('utf-8')})
 print(resp.json())
